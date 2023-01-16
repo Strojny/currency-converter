810f408b93af1f74bfb0de2bddd39d8cc94765d4
Sebastian Strojny <sebastian.strojny@gmail.com>
2023-01-16 10:40:23 +0100

    functions to arrow functions refactoring

--- js/script.js
+++ js/script.js
@@ -1,22 +1,27 @@
-let form = document.querySelector(".js-formElement");
-let eurValue = document.querySelector(".js-eurValueElement");
-let usdValue = document.querySelector(".js-usdValueElement");
-let gbpValue = document.querySelector(".js-gbpValueElement");
-let currencyAmount = document.querySelector(".js-currencyAmountElement");
-let plnValue = document.querySelector(".js-plnValueElement");
+{
+    const calculateResult = (currencyAmount) => {
+        const eurValue = document.querySelector(".js-eurValueElement");
+        const usdValue = document.querySelector(".js-usdValueElement");
+        /* name of below constant (currencyAmount) should be different than function parameter?? */
+        const currencyAmount = document.querySelector(".js-currencyAmountElement");
 
-form.addEventListener("submit", (event) => {
-    event.preventDefault();
-    let result;
+        if (eurValue.checked === true) {
+            return 4.75 * +currencyAmount.value;
+        } else if (usdValue.checked === true) {
+            return 4.74 * +currencyAmount.value;
+        } else {
+            return 5.48 * +currencyAmount.value;
+        };
+    };
 
-    if (eurValue.checked === true) {
-        result = 4.75 * +currencyAmount.value;
-    } else if (usdValue.checked === true) {
-        result = 4.74 * +currencyAmount.value;
-    } else {
-        result = 5.48 * +currencyAmount.value;
-    }
+    const form = document.querySelector(".js-formElement");
 
-    plnValue.innerText = result.toFixed(2);
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
 
-});
+        const plnValue = document.querySelector(".js-plnValueElement");
+        const result = calculateResult(currencyAmount);
+        plnValue.innerText = result.toFixed(2);
+
+    });
+}
